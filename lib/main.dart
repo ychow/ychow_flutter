5461ee1f171bacc932d569124cf07174b73a9733
ychow xiao <yuanzhouxiao@nobleapex.com.hk>
2019-04-13 22:29:58 +0800

    feat: add basic_demo

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'demo/ListView_demo.dart';
 import 'demo/Drawer_demo.dart';
 import 'demo/Bottom_navigation_bar_demo.dart';
+import 'demo/Basic_demo.dart';
 
 void main() => runApp(App());
 
@@ -59,7 +60,8 @@ class Home extends StatelessWidget {
         body: TabBarView(
           children: <Widget>[
             ListViewDemo(),
-            Icon(Icons.change_history, size: 128.0, color: Colors.black12),
+            // Icon(Icons.change_history, size: 128.0, color: Colors.black12),
+            BasicDemo(),
             Icon(Icons.directions_bike, size: 128.0, color: Colors.black12),
           ],
         ),
